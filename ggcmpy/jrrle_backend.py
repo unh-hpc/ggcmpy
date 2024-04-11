44440b8e8a16d0f1720288f12dc46515bdfdf213
Kai Germaschewski <kai.germaschewski@unh.edu>
2024-04-11 09:43:31 -0400

    jrrle: parse coordinates, timestr

--- ggcmpy/jrrle_backend.py
+++ ggcmpy/jrrle_backend.py
@@ -1,8 +1,9 @@
 import xarray as xr
 from xarray.backends import BackendEntrypoint
 import numpy as np
-
+from itertools import islice
 import os
+import re
 
 from ggcmpy.ggcm_jrrle import JrrleFileWrapper
 
@@ -38,12 +39,109 @@ class JrrleEntrypoint(BackendEntrypoint):
     url = "https://link_to/your_backend/documentation"  # FIXME
 
 
+def _jrrle_parse_filename(filename):
+    dirname = os.path.dirname(filename)
+    run, ifx, step = os.path.basename(filename).split(".")
+    return dict(dirname=dirname, run=run, ifx=ifx, step=int(step))
+
+
+def _jrrle_read_grid2(filename):
+    meta = _jrrle_parse_filename(filename)
+    filename = os.path.join(meta['dirname'], f"{meta['run']}.grid2")
+    # load the cell centered grid
+    with open(filename, 'r') as fin:
+        nx = int(next(fin).split()[0])
+        gx = list(islice(fin, 0, nx, 1))
+        gx = np.array(gx, dtype='f4')
+
+        ny = int(next(fin).split()[0])
+        gy = list(islice(fin, 0, ny, 1))
+        gy = np.array(gy, dtype='f4')
+
+        nz = int(next(fin).split()[0])
+        gz = list(islice(fin, 0, nz, 1))
+        gz = np.array(gz, dtype='f4')
+
+    return {"x": gx, "y": gy, "z": gz}
+
+
+def as_isotime(time):
+    """Try to convert times in string format to ISO 8601
+
+    Raises:
+        TypeError: Elements are not strings
+        ValueError: numpy.datetime64(time) fails
+    """
+    if isinstance(time, (list, tuple, np.ndarray)):
+        scalar = False
+    else:
+        scalar = True
+        time = [time]
+
+    ret = [None] * len(time)
+    for i, t in enumerate(time):
+        t = t.strip().upper().lstrip('UT')
+        if re.match(r"^[0-9]{2}([0-9]{2}:){3,5}[0-9]{1,2}(\.[0-9]*)?$", t):
+            # Handle YYYY:MM:DD:hh:mm:ss.ms -> YYYY-MM-DDThh:mm:ss.ms
+            #        YYYY:MM:DD:hh:mm:s.ms  -> YYYY-MM-DDThh:mm:s.ms
+            #        YYYY:MM:DD:hh:mm:ss    -> YYYY-MM-DDThh:mm:ss
+            #        YYYY:MM:DD:hh:mm       -> YYYY-MM-DDThh:mm
+            #        YYYY:MM:DD:hh          -> YYYY-MM-DDThh
+            # -- all this _tsp nonsense is to take care of s.ms; annoying
+            _tsp = t.replace('.', ':').split(':')
+            _tsp[0] = _tsp[0].zfill(4)
+            _tsp[1:6] = [_s.zfill(2) for _s in _tsp[1:6]]
+            t = ":".join(_tsp[:6])
+            if len(_tsp) > 6:
+                t += "." + _tsp[6]
+            # --
+            ret[i] = t[:10].replace(':', '-') + 'T' + t[11:]
+
+        elif re.match(r"^[0-9]{2}([0-9]{2}:){2}[0-9]{2}$", t):
+            # Handle YYYY:MM:DD -> YYYY-MM-DD
+            ret[i] = t.replace(':', '-')
+        else:
+            ret[i] = t
+
+        try:
+            np.datetime64(ret[i])
+        except ValueError:
+            raise
+
+    if scalar:
+        return ret[0]
+    else:
+        if isinstance(time, np.ndarray):
+            return np.array(time, dtype=time.dtype)
+        else:
+            return ret
+
+
+def _openggcm_parse_timestring(timestr):
+    prefix = 'time='
+    timestr.strip()
+    if not timestr.startswith(prefix):
+        raise ValueError("Time string '{0}' is malformed".format(timestr))
+    timestr = timestr[len(prefix):].split()
+    t = float(timestr[0])
+    t = np.timedelta64(int(1000*t), "ms")
+    uttime = np.datetime64(as_isotime(timestr[2]))
+    print(f"t = {t}")
+    print(f"uttime {uttime} {type(uttime)}")
+    return t, uttime
+
+
 def jrrle_open_dataset(filename_or_obj, *, drop_variables=None):
+    meta = _jrrle_parse_filename(filename_or_obj)
+
+    coords = _jrrle_read_grid2(filename_or_obj)
+    dims = coords["x"].shape[0], coords["y"].shape[0], coords["z"].shape[0]
+
+    attrs = dict(run=meta["run"], dims=dims)
 
     file_wrapper = JrrleFileWrapper(filename_or_obj)
     file_wrapper.open()
     file_wrapper.inquire_all_fields()
-    file_wrapper.rewind()  # FIXME why?
 
     with file_wrapper as f:
         flds = f.fields_seen
@@ -51,19 +149,21 @@ def jrrle_open_dataset(filename_or_obj, *, drop_variables=None):
         for fld in flds.keys():
             ndim = flds[fld]["ndim"]
             meta, arr = f.read_field(fld, ndim)
-            # dims = meta["dims"]
-            # arr = np.array(arr.flatten(order='F').reshape(dims),  # [::-1]),
-            #                order='F')
+            assert meta["dims"] == dims
+            time, uttime = _openggcm_parse_timestring(meta["timestr"])
 
-            vars[fld] = ("x", "y", "z"), arr
-        # vars[fld] = xr.DataArray(
-        #     data=arr,
-        #     dims=["x", "y", "z"])
+            data_attrs = dict(
+                inttime=meta["inttime"], time=time, uttime=uttime)
+
+            vars[fld] = xr.DataArray(
+                data=arr,
+                dims=["x", "y", "z"],
+                attrs=data_attrs)
         # vars, attrs, coords = my_decode_variables(
         #     vars, attrs, decode_times, decode_timedelta, decode_coords
         # )  #  see also conventions.decode_cf_variables
 
-    ds = xr.Dataset(vars)  # , attrs=attrs, coords=coords)
+    ds = xr.Dataset(vars, coords=coords, attrs=attrs)
 #    ds.set_close(my_close_method)
 
     return ds
