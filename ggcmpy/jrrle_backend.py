4365eb74850c071db97845f409a68ee3c562874b
Kai Germaschewski <kai.germaschewski@gmail.com>
2024-12-16 21:03:55 -0500

    ggcmpy: make jrrle reader compatible with ggcm-gitm-coupling-tools

--- ggcmpy/jrrle_backend.py
+++ ggcmpy/jrrle_backend.py
@@ -56,12 +56,13 @@ def jrrle_open_dataset(filename_or_obj, *, drop_variables=None):
     elif meta["type"] == "3df":
         data_dims = ["x", "y", "z"]
     elif meta["type"] == "iof":
-        data_dims = ["lon", "lat"]
+        data_dims = ["longs", "lats"]
 
     file_wrapper = jrrle.JrrleFile(filename_or_obj)
     file_wrapper.open()
     file_wrapper.inquire_all_fields()
 
+    time = None
     with file_wrapper as f:
         flds = f.fields_seen
         vars = {}
@@ -70,8 +71,14 @@ def jrrle_open_dataset(filename_or_obj, *, drop_variables=None):
             fld_info, arr = f.read_field(fld, ndim)
             if not dims:
                 dims = fld_info["dims"]
-            time, uttime = openggcm.parse_timestring(fld_info["timestr"])
-            data_attrs = dict(inttime=fld_info["inttime"], time=time, uttime=uttime)
+            data_attrs = dict(inttime=fld_info["inttime"])
+            data_attrs.update(openggcm.parse_timestring(fld_info["timestr"]))
+
+            if time is not None:
+                assert (
+                    time == data_attrs["time"]
+                ), "inconsistent time info in jrrle file"
+            time = data_attrs["time"]
 
             vars[fld] = xr.DataArray(data=arr, dims=data_dims, attrs=data_attrs)
         # vars, attrs, coords = my_decode_variables(
@@ -80,11 +87,13 @@ def jrrle_open_dataset(filename_or_obj, *, drop_variables=None):
 
     if meta["type"] == "iof":
         coords = dict(
-            lat=np.linspace(90.0, -90.0, dims[1]),
-            lon=np.linspace(-180.0, 180.0, dims[0]),
+            lats=("lats", np.linspace(90.0, -90.0, dims[1])),
+            colats=("lats", np.linspace(0.0, 180.0, dims[1])),
+            longs=("longs", np.linspace(-180.0, 180.0, dims[0])),
+            mlts=("longs", np.linspace(0.0, 24.0, dims[0])),
         )
 
-    attrs = dict(run=meta["run"], dims=dims)
+    attrs = dict(run=meta["run"], dims=dims, time=time)
 
     ds = xr.Dataset(vars, coords=coords, attrs=attrs)
     #    ds.set_close(my_close_method)
