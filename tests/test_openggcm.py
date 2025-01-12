e27e21676a1b5c62d4026c49073aad04f836f089
Kai Germaschewski <kai.germaschewski@gmail.com>
2025-01-12 11:47:40 -0500

    openggcm: test decode for arrays and scalars

--- tests/test_openggcm.py
+++ tests/test_openggcm.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from ggcmpy import openggcm
 import xarray as xr
 import numpy as np
+import pytest
 
 sample_time_array = np.array([[2010, 1, 1, 13, 0, 0, 100], [2010, 1, 1, 13, 1, 0, 100]])
 sample_datetime64 = [
@@ -22,9 +23,16 @@ def test_to_time_array():
     )
 
 
-def test_decode_openggcm_variable():
-    var = xr.Variable(("time", "time_array"), sample_time_array)
-    var_dt64 = xr.Variable("time", sample_datetime64)
+@pytest.mark.parametrize(
+    ("dims", "data_time_array", "data_datetime64"),
+    [
+        (("time", "time_array"), sample_time_array, sample_datetime64),
+        (("time_array",), sample_time_array[0], sample_datetime64[0]),
+    ],
+)
+def test_decode_openggcm_variable(dims, data_time_array, data_datetime64):
+    var = xr.Variable(dims, data_time_array)
+    var_dt64 = xr.Variable(dims[:-1], data_datetime64)
 
     decoded_var = openggcm._decode_openggcm_variable(var, "name")
     assert decoded_var.equals(var)
