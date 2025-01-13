eb54b84f6f38670b453714e5fb82148f77b8d423
Kai Germaschewski <kai.germaschewski@gmail.com>
2025-01-13 17:47:06 -0500

    jrrle/file: add tests for inquire and read_field()

--- tests/test_jrrle.py
+++ tests/test_jrrle.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import hashlib
 import pathlib
 
 import pytest
@@ -27,3 +28,31 @@ def test_jrrle_file_iter():
         for (var_name, _), ref_name in zip(file, sample_iof["data_vars"], strict=False):
             assert var_name == ref_name
             file.advance_one_line()
+
+
+def test_jrrle_file_inquire():
+    vars: list[str] = sample_iof["data_vars"]  # type: ignore[assignment]
+    with JrrleFile(pathlib.Path(ggcmpy.sample_dir) / "coupling0001.iof.000030") as file:
+        for var_name in vars[:3]:
+            file.inquire(var_name)
+        assert vars[1] in file.fields_seen
+        assert vars[3] not in file.fields_seen
+        file.inquire(vars[3])
+        assert vars[3] in file.fields_seen
+
+
+def test_jrrle_file_read_field():
+    vars: list[str] = sample_iof["data_vars"]  # type: ignore[assignment]
+    with JrrleFile(pathlib.Path(ggcmpy.sample_dir) / "coupling0001.iof.000030") as file:
+        meta, var0 = file.read_field(vars[0], 2)
+        assert hashlib.sha256(var0.tobytes()).hexdigest().startswith("6f8b81d9")
+        meta, var1 = file.read_field(vars[1], 2)
+        assert hashlib.sha256(var1.tobytes()).hexdigest().startswith("572fed56")
+        meta, var2 = file.read_field(vars[2], 2)
+        assert hashlib.sha256(var2.tobytes()).hexdigest().startswith("cd58c4af")
+
+        meta, var1 = file.read_field(vars[1], 2)
+        assert hashlib.sha256(var1.tobytes()).hexdigest().startswith("572fed56")
+
+        meta, var3 = file.read_field(vars[3], 2)
+        assert hashlib.sha256(var3.tobytes()).hexdigest().startswith("7960c540")
