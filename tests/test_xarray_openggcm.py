f509b069fe8d788135d59b2ec172ee9971a4cc7e
Kai Germaschewski <kai.germaschewski@gmail.com>
2025-01-13 09:57:21 -0500

    jrrle: add test for open_mfdataset

--- tests/test_xarray_openggcm.py
+++ tests/test_xarray_openggcm.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import numpy as np
 import xarray as xr
 
 import ggcmpy
@@ -35,9 +36,23 @@ iof_vars = {
     "cpolp",
     "ctiop",
 }
+iof_times = np.asarray(
+    ["1967-01-01T00:00:30.150", "1967-01-01T00:01:00.101"], dtype=np.datetime64
+)
 
 
 def test_open_dataset():
     ds = xr.open_dataset(f"{ggcmpy.sample_dir}/coupling0001.iof.000030")
     assert set(ds.coords.keys()) == iof_coords
     assert set(ds.keys()) == iof_vars
+    assert ds.time == iof_times[0]
+
+
+def test_read_iof_jrrle_mfdataset():
+    filenames = [
+        f"{ggcmpy.sample_dir}/coupling0001.iof.{step:06d}" for step in [30, 60]
+    ]
+    ds = xr.open_mfdataset(filenames)
+    assert set(ds.coords.keys()) == iof_coords
+    assert set(ds.keys()) == iof_vars
+    assert np.all(ds.time.to_numpy() == iof_times)
