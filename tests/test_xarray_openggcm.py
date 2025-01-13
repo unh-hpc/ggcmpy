ed8c3c9d2d6ccc342ca28f1d35c7c4f1292ae9f8
Kai Germaschewski <kai.germaschewski@gmail.com>
2025-01-13 10:30:42 -0500

    jrrle: put all ref info into "sample_iof"

--- tests/test_xarray_openggcm.py
+++ tests/test_xarray_openggcm.py
@@ -5,47 +5,49 @@ import xarray as xr
 
 import ggcmpy
 
-iof_coords = {"lats", "longs", "time"}
-iof_vars = {
-    "pot",
-    "sigp",
-    "sigh",
-    "rrio",
-    "ppio",
-    "ttio",
-    "prec_e_e0_1",
-    "prec_e_fe_1",
-    "prec_e_e0_2",
-    "prec_e_fe_2",
-    "fac_dyn",
-    "fac_tot",
-    "vdown",
-    "ctaut",
-    "delphi",
-    "tau",
-    "pacurr",
-    "xjh",
-    "delbp",
-    "epio",
-    "ctiot",
-    "delbr",
-    "ctaup",
-    "etio",
-    "delbt",
-    "cpolt",
-    "cpolp",
-    "ctiop",
+sample_iof = {
+    "coords": {"lats", "longs", "time"},
+    "data_vars": {
+        "pot",
+        "sigp",
+        "sigh",
+        "rrio",
+        "ppio",
+        "ttio",
+        "prec_e_e0_1",
+        "prec_e_fe_1",
+        "prec_e_e0_2",
+        "prec_e_fe_2",
+        "fac_dyn",
+        "fac_tot",
+        "vdown",
+        "ctaut",
+        "delphi",
+        "tau",
+        "pacurr",
+        "xjh",
+        "delbp",
+        "epio",
+        "ctiot",
+        "delbr",
+        "ctaup",
+        "etio",
+        "delbt",
+        "cpolt",
+        "cpolp",
+        "ctiop",
+    },
+    "time": np.asarray(
+        ["1967-01-01T00:00:30.150", "1967-01-01T00:01:00.101"], dtype=np.datetime64
+    ),
 }
-iof_times = np.asarray(
-    ["1967-01-01T00:00:30.150", "1967-01-01T00:01:00.101"], dtype=np.datetime64
-)
 
 
-def test_open_dataset():
+def test_open_dataset_iof():
     ds = xr.open_dataset(f"{ggcmpy.sample_dir}/coupling0001.iof.000030")
-    assert set(ds.coords.keys()) == iof_coords
-    assert set(ds.keys()) == iof_vars
-    assert ds.time == iof_times[0]
+    assert set(ds.coords.keys()) == sample_iof["coords"]
+    assert set(ds.keys()) == sample_iof["data_vars"]
+    assert ds.time == sample_iof["time"][0]  # type: ignore[index]
 
 
 def test_read_iof_jrrle_mfdataset():
@@ -53,6 +55,6 @@ def test_read_iof_jrrle_mfdataset():
         f"{ggcmpy.sample_dir}/coupling0001.iof.{step:06d}" for step in [30, 60]
     ]
     ds = xr.open_mfdataset(filenames)
-    assert set(ds.coords.keys()) == iof_coords
-    assert set(ds.keys()) == iof_vars
-    assert np.all(ds.time.to_numpy() == iof_times)
+    assert set(ds.coords.keys()) == sample_iof["coords"]
+    assert set(ds.keys()) == sample_iof["data_vars"]
+    assert np.all(ds.time == sample_iof["time"])
