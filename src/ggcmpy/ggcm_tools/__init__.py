cd591e719f721f0da8c391310166fc167349d06d
Kai Germaschewski <kai.germaschewski@gmail.com>
2025-02-14 10:40:18 -0500

    ggcm_tools: remove amr_tree_tools

--- src/ggcmpy/ggcm_tools/__init__.py
+++ src/ggcmpy/ggcm_tools/__init__.py
@@ -1,17 +1,3 @@
-# bring some AMR stuff into the ggcm_tools namespace
-from __future__ import absolute_import
-
-from . import amr_tree_tools
-
-from .amr_tree_tools import Patch
-from .amr_tree_tools import (Region, SphereRegion, RectRegion,
-                                       PointRegion, UnionRegion,
-                                       IntersectionRegion, SubtractionRegion,
-                                       InvertedRegion)
-from .amr_tree_tools import run_refinement
-from .amr_tree_tools import (read_tree_libmrc, dump_tree_info,
-                                       dump_tree_libmrc)
 
 # bring some CDAWeb stuff into the ggcm_tools namespace
-from . import CDAfetch
-from . import CDAWeb
+from . import CDAfetch, CDAWeb
