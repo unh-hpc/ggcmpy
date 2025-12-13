299d51bb03cba1c7c1499b1ab999013b0036df16
Kai Germaschewski <kai.germaschewski@gmail.com>
2025-12-13 14:31:46 -0500

    sw: add sample data for tests

--- tests/test_timeseries.py
+++ tests/test_timeseries.py
@@ -10,9 +10,7 @@ from ggcmpy.timeseries import read_ggcm_solarwind_file
 
 
 def test_read_ggcm_solarwind_file():
-    rr = read_ggcm_solarwind_file(
-        ggcmpy.sample_dir / "cir07_19970227_liang_norcm/tmp.minvar/om1997.rr"
-    )
+    rr = read_ggcm_solarwind_file(ggcmpy.sample_dir / "om1997/om1997.rr")
     assert set(rr.columns) == {"om1997.rr"}
     assert pd.api.types.is_datetime64_ns_dtype(rr.index)
     assert np.isclose(rr["om1997.rr"].mean(), 4.8191107)
@@ -20,7 +18,7 @@ def test_read_ggcm_solarwind_file():
 
 def test_read_ggcm_solarwind_directory():
     bfield = ggcmpy.timeseries.read_ggcm_solarwind_directory(
-        ggcmpy.sample_dir / "cir07_19970227_liang_norcm/tmp.minvar", glob="om1997.b*gse"
+        ggcmpy.sample_dir / "om1997", glob="om1997.b*gse"
     )
     assert set(bfield.columns) == {"om1997.bxgse", "om1997.bygse", "om1997.bzgse"}
     assert pd.api.types.is_datetime64_ns_dtype(bfield.index)
@@ -31,7 +29,7 @@ def test_store_to_pyspedas_pandas():
     pyspedas = pytest.importorskip("pyspedas")
 
     bfield = ggcmpy.timeseries.read_ggcm_solarwind_directory(
-        ggcmpy.sample_dir / "cir07_19970227_liang_norcm/tmp.minvar", glob="om1997.b*gse"
+        ggcmpy.sample_dir / "om1997", glob="om1997.b*gse"
     )
     ggcmpy.timeseries.store_to_pyspedas(bfield)
     assert "om1997.bxgse" in pyspedas.tplot_names()
