af9b3ec0981f9dd3d7fd6e356c52769c44ebf413
Kai Germaschewski <kai.germaschewski@gmail.com>
2026-02-18 17:56:53 +0000

    tests: fix the expected datatype for time stamp

--- tests/test_timeseries.py
+++ tests/test_timeseries.py
@@ -14,7 +14,7 @@ def test_read_ggcm_solarwind_file():
         ggcmpy.sample_dir / "cir07_19970227_liang_norcm/tmp.minvar/om1997.rr"
     )
     assert set(rr.columns) == {"om1997.rr"}
-    assert pd.api.types.is_datetime64_ns_dtype(rr.index)
+    assert isinstance(rr.index[0], pd.Timestamp)
     assert np.isclose(rr["om1997.rr"].mean(), 4.8191107)
 
 
@@ -23,7 +23,7 @@ def test_read_ggcm_solarwind_directory():
         ggcmpy.sample_dir / "cir07_19970227_liang_norcm/tmp.minvar", glob="om1997.b*gse"
     )
     assert set(bfield.columns) == {"om1997.bxgse", "om1997.bygse", "om1997.bzgse"}
-    assert pd.api.types.is_datetime64_ns_dtype(bfield.index)
+    assert isinstance(bfield.index[0], pd.Timestamp)
     assert np.isclose(bfield["om1997.bzgse"].mean(), -1.87348468)
 
 
