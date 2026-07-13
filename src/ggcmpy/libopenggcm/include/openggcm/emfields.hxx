17bc6b863cb6a7ed16fcf0ca43fd01402b560578
Kai Germaschewski <kai.germaschewski@gmail.com>
2026-07-13 11:05:08 -0400

    tracing: emfields_constant -> emfields_uniform etc
    consistency with python naming

--- src/ggcmpy/libopenggcm/include/openggcm/emfields.hxx
+++ src/ggcmpy/libopenggcm/include/openggcm/emfields.hxx
@@ -6,20 +6,20 @@
 namespace openggcm
 {
 
-class emfields_constant
+class emfields_uniform
 {
 public:
-  emfields_constant(std::array<double, 3> E0, std::array<double, 3> B0)
-    : _E0(E0), _B0(B0)
+  emfields_uniform(std::array<double, 3> E_0, std::array<double, 3> B_0)
+    : _E_0(E_0), _B_0(B_0)
   {
   }
 
-  std::array<double, 3> E(double x, double y, double z) const { return _E0; }
-  std::array<double, 3> B(double x, double y, double z) const { return _B0; }
+  std::array<double, 3> E(double x, double y, double z) const { return _E_0; }
+  std::array<double, 3> B(double x, double y, double z) const { return _B_0; }
 
 private:
-  std::array<double, 3> _E0;
-  std::array<double, 3> _B0;
+  std::array<double, 3> _E_0;
+  std::array<double, 3> _B_0;
 };
 
 } // namespace openggcm
--- src/ggcmpy/libopenggcm/tests/test_emfields.cxx
+++ src/ggcmpy/libopenggcm/tests/test_emfields.cxx
@@ -5,10 +5,10 @@
 
 TEST(EmfieldsTest, ConstantFields)
 {
-  std::array<double, 3> E0 = {1.0, 2.0, 3.0};
-  std::array<double, 3> B0 = {4.0, 5.0, 6.0};
-  openggcm::emfields_constant fields(E0, B0);
+  std::array<double, 3> E_0 = {1.0, 2.0, 3.0};
+  std::array<double, 3> B_0 = {4.0, 5.0, 6.0};
+  openggcm::emfields_uniform fields(E_0, B_0);
 
-  EXPECT_EQ(fields.E(0.0, 0.0, 0.0), E0);
-  EXPECT_EQ(fields.B(0.0, 0.0, 0.0), B0);
+  EXPECT_EQ(fields.E(0.0, 0.0, 0.0), E_0);
+  EXPECT_EQ(fields.B(0.0, 0.0, 0.0), B_0);
 }
