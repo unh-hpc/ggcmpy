8f6ec725db90a9e9f1b942560a40e51bdf52beff
Kai Germaschewski <kai.germaschewski@gmail.com>
2026-07-21 12:54:06 -0400

    tracing: add python test for emfields.uniform_cxx

--- src/ggcmpy/libopenggcm/nanobind/bind.cxx
+++ src/ggcmpy/libopenggcm/nanobind/bind.cxx
@@ -10,10 +10,20 @@ using namespace nb::literals;
 
 NB_MODULE(_openggcm, m)
 {
-  nb::class_<openggcm::emfields_constant>(m, "emfields_constant")
-    .def(nb::init<std::array<double, 3>, std::array<double, 3>>(), "E0"_a,
-         "B0"_a)
-    .def("E", &openggcm::emfields_constant::E, "x"_a, "y"_a, "z"_a)
-    .def("B", &openggcm::emfields_constant::B, "x"_a, "y"_a, "z"_a)
-    .def("__repr__", &openggcm::emfields_constant::repr);
+  nb::class_<openggcm::emfields_uniform>(m, "emfields_uniform")
+    .def(nb::init<std::array<double, 3>, std::array<double, 3>>(), "E_0"_a,
+         "B_0"_a)
+    .def(
+      "E",
+      [](openggcm::emfields_uniform& self, std::array<double, 3> r) {
+        return self.E(r[0], r[1], r[2]);
+      },
+      "r"_a)
+    .def(
+      "B",
+      [](openggcm::emfields_uniform& self, std::array<double, 3> r) {
+        return self.B(r[0], r[1], r[2]);
+      },
+      "r"_a)
+    .def("__repr__", &openggcm::emfields_uniform::repr);
 }
--- tests/test_emfields.py
+++ tests/test_emfields.py
@@ -59,8 +59,15 @@ class emfields_test:
         )
 
 
-def test_emfields_uniform():
-    emfields = tracing.emfields.uniform(E_0=[1.0, 2.0, 3.0], B_0=[4.0, 5.0, 6.0])
+@pytest.mark.parametrize(
+    "emfields_uniform",
+    [
+        ggcmpy.tracing.emfields.uniform_python,
+        ggcmpy.tracing.emfields.uniform_cxx,
+    ],
+)
+def test_emfields_uniform(emfields_uniform):
+    emfields = emfields_uniform(E_0=[1.0, 2.0, 3.0], B_0=[4.0, 5.0, 6.0])
     pos = [6.0, 7.0, 8.0]
     assert np.allclose(emfields.E(pos), [1.0, 2.0, 3.0])
     assert np.allclose(emfields.B(pos), [4.0, 5.0, 6.0])
