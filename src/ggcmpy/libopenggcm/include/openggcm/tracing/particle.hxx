a03bb46220768a3af23acc4118324ab2890adb93
Kai Germaschewski <kai.germaschewski@gmail.com>
2026-08-04 17:09:37 +0800

    tracing/cxx/particle: no more `particle`

--- src/ggcmpy/libopenggcm/include/openggcm/tracing/particle.hxx
+++ src/ggcmpy/libopenggcm/include/openggcm/tracing/particle.hxx
@@ -9,29 +9,6 @@ namespace openggcm
 namespace tracing
 {
 
-class particle
-{
-public:
-  particle(double3 x, double3 u) : x(x), u(u) {}
-
-  double3 v() const { return (constants::c / gamma()) * u; }
-
-  double gamma() const
-  {
-    return std::sqrt(1.0 + sqr(u[0]) + sqr(u[1]) + sqr(u[2]));
-  }
-
-  std::string repr() const
-  {
-    return "particle(x=[" + std::to_string(x[0]) + ", " + std::to_string(x[1]) +
-           ", " + std::to_string(x[2]) + "], u=[" + std::to_string(u[0]) +
-           ", " + std::to_string(u[1]) + ", " + std::to_string(u[2]) + "])";
-  }
-
-  double3 x;
-  double3 u;
-};
-
 class particles
 {
 public:
